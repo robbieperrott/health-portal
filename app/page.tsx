5d7fe3fd0996842775f4b8f74580db87ea2bc042
Robbie Perrott <robbieperrott@gmail.com>
2025-09-27 11:04:32 +0200

    Add basic home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function Home() {
 
   return (
-    <div>
-    </div>
+    <Card className="max-w-sm h-fit">
+      <CardHeader>
+        <CardTitle className="text-xl font-bold text-center">Welcome to your health portal</CardTitle>
+      </CardHeader>
+      <CardContent>
+        Click on one of the links in the navigation menu above to explore your data or chat to our AI assistant.
+      </CardContent>
+    </Card>
   );
 }
