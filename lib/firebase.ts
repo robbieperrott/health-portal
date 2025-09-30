e5168e4752fec6054162affe9c7fde067cce77b9
Robbie Perrott <robbieperrott@gmail.com>
2025-09-30 08:42:04 +0200

    Update firebase config to use env variables and check that app has not already been initialised

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,14 +1,16 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAviqm08xWLhNfrXGfYQGtAOBA9Y0ONvAQ",
-  authDomain: "nuraxi-fcb5f.firebaseapp.com",
-  projectId: "nuraxi-fcb5f",
-  storageBucket: "nuraxi-fcb5f.firebasestorage.app",
-  messagingSenderId: "280176162641",
-  appId: "1:280176162641:web:6b610c6394dd1316334570",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
 export const auth = getAuth(app);
+export default app;
